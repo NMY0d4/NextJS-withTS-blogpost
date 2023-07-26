cf3932e8d63ca321663d126be5e225cdf8dd7bb2
Grégory Marini <greg.marini@hotmail.fr>
2023-07-26 14:25:55 +0200

    add not-found page and generateMetadata function for post detail

--- app/posts.tsx/[postId]/page.tsx
+++ app/posts.tsx/[postId]/page.tsx
@@ -2,6 +2,17 @@ import { getSortedPostsData } from '@/lib/posts';
 import React from 'react';
 import { notFound } from 'next/navigation';
 
+export function generateMetadata({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData();
+  const { postId } = params;
+
+  const post = posts.find((post) => post.id === postId);
+
+  return {
+    title: post?.title || 'Post Not found',
+  };
+}
+
 export default async function Post({ params }: { params: { postId: string } }) {
   const posts = getSortedPostsData();
   const { postId } = params;
